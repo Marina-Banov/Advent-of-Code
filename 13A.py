7c89eb9a0c0b3c0dc15f55cb3cb958d36626723f
mbanov <m.banov7@gmail.com>
2020-12-16 21:22:00 +0100

    Day 13

---
+++ 13A.py
@@ -0,0 +1,13 @@
+import math
+
+
+f = open("input.txt", "r")
+estimate = int(f.readline().strip())
+buses = list(map(int, filter(lambda a: a != 'x', f.readline().strip().split(','))))
+min_estimate = math.inf
+min_bus = 0
+for bus in buses:
+    if (estimate // bus + 1) * bus < min_estimate:
+        min_estimate = (estimate // bus + 1) * bus
+        min_bus = bus
+print((min_estimate - estimate) * min_bus)
