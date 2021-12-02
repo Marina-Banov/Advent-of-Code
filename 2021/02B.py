27db6ce1317579eef323f073b5b48d4ee99f2d46
mbanov <m.banov7@gmail.com>
2021-12-02 12:25:34 +0100

    Day 2

---
+++ 2021/02B.py
@@ -0,0 +1,17 @@
+f = open("input.txt", "r")
+horizontal = 0
+aim = 0
+depth = 0
+while True:
+    try:
+        direction, value = f.readline().strip().split()
+        if direction == 'forward':
+            horizontal += int(value)
+            depth += int(value) * aim
+        elif direction == 'up':
+            aim -= int(value)
+        else:
+            aim += int(value)
+    except Exception as e:
+        break
+print(horizontal * depth)
