37c0c8716b6d2dbb0a9e9f9b08c385760f63c56e
mbanov <m.banov7@gmail.com>
2021-12-02 12:07:40 +0100

    Day 1

---
+++ 2021/01A.py
@@ -0,0 +1,12 @@
+f = open("input.txt", "r")
+count = 0
+prev = None
+while True:
+    try:
+        curr = int(f.readline().strip())
+        if prev is not None and prev < curr:
+            count += 1
+        prev = curr
+    except Exception as e:
+        break
+print(count)
---
+++ 2021/01B.py
@@ -0,0 +1,13 @@
+f = open("input.txt", "r")
+values = f.readlines()
+count = 0
+prev = None
+for i in range(len(values)-2):
+    a = int(values[i].strip())
+    b = int(values[i+1].strip())
+    c = int(values[i+2].strip())
+    curr = a + b + c
+    if prev is not None and prev < curr:
+        count += 1
+    prev = curr
+print(count)
