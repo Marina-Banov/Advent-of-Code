9eb6211135193359d7e2957b61403a7dca09d0f5
mbanov <m.banov7@gmail.com>
2021-12-11 11:45:50 +0100

    Day 11

---
+++ 2021/11B.py
@@ -0,0 +1,32 @@
+def get_adjacent(i, j):
+    DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    return [(i+d[0], j+d[1]) for d in DIRECTIONS if 0 <= i+d[0] < 10 and 0 <= j+d[1] < 10]
+
+
+def get_flash_indices():
+    return [(i, j) for i, row in enumerate(matrix) for j, val in enumerate(row) if val >= 10]
+
+
+def increase_energy():
+    return [[octopus+1 for octopus in row] for row in matrix]
+
+
+f = open("input.txt", "r")
+
+matrix = [list(map(int, line.strip())) for line in f]
+adjacent = [get_adjacent(i, j) for i in range(10) for j in range(10)]
+step = 0
+
+while sum([val for row in matrix for val in row]) > 0:
+    matrix = increase_energy()
+    flashes = get_flash_indices()
+    while len(flashes) > 0:
+        for (i, j) in flashes:
+            matrix[i][j] = 0
+            for (a_i, a_j) in adjacent[i*10 + j]:
+                if matrix[a_i][a_j] != 0:
+                    matrix[a_i][a_j] += 1
+        flashes = get_flash_indices()
+    step += 1
+
+print(step)
