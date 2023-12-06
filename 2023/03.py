b5d99ba12d4c0d218c4a4e078707e21fbaaf31de
mbanov <m.banov7@gmail.com>
2023-12-06 08:30:40 +0100

    Day 3

---
+++ 2023/03.py
@@ -0,0 +1,77 @@
+def get_part_numbers(trace, schematic):
+    trace.sort()
+    part_numbers = []
+    p = []
+    for i, j in trace:
+        if not schematic[i][j].isdigit():
+            continue
+        if len(p) == 0 or (i == p[-1][0] and j == p[-1][1] + 1):
+            p.append((i, j))
+        else:
+            part_numbers.append(int("".join([schematic[i][j] for i, j in p])))
+            p = [(i, j)]
+    part_numbers.append(int("".join([schematic[i][j] for i, j in p])))
+    return part_numbers
+
+
+def part_one(trace, schematic):
+    if all(schematic[i][j].isdigit() for i, j in trace):
+        return 0
+    return sum(get_part_numbers(trace, schematic))
+
+
+def part_two(trace, schematic):
+    if "*" not in [schematic[i][j] for i, j in trace]:
+        return 0
+    part_numbers = get_part_numbers(trace, schematic)
+    return part_numbers[0] * part_numbers[1] if len(part_numbers) == 2 else 0
+
+
+def in_range(i, j, max_rows, max_cols):
+    return 0 <= i < max_rows and 0 <= j < max_cols
+
+
+def adjacent_d(matrix, row, col, _filter=lambda *_: True):
+    di = [-1, -1, -1,  0, 0,  1, 1, 1]
+    dj = [-1,  0,  1, -1, 1, -1, 0, 1]
+    n_rows = len(matrix)
+    n_cols = len(matrix[0])
+    return [
+        (row + di[d], col + dj[d]) for d in range(8)
+        if in_range(row + di[d], col + dj[d], n_rows, n_cols)
+        and _filter(matrix, row + di[d], col + dj[d])
+    ]
+
+
+def is_not_dot(matrix, i, j):
+    return matrix[i][j] != "."
+
+
+def dfs(matrix, ui, uj, visited, trace):
+    visited[ui][uj] = True
+    for vi, vj in adjacent_d(matrix, ui, uj, is_not_dot):
+        if not visited[vi][vj]:
+            trace.append((vi, vj))
+            dfs(matrix, vi, vj, visited, trace)
+
+
+def main(sum_el):
+    with open("input.txt", "r") as f:
+        schematic = [list(l.strip()) for l in f.readlines()]
+    n_rows = len(schematic)
+    n_cols = len(schematic[0])
+    visited = [[False for _ in range(n_rows)] for _ in range(n_cols)]
+    res = 0
+    for row in range(n_rows):
+        for col in range(n_cols):
+            if not schematic[row][col].isdigit() or visited[row][col]:
+                continue
+            trace = [(row, col)]
+            dfs(schematic, row, col, visited, trace)
+            res += sum_el(trace, schematic)
+    return res
+
+
+if __name__ == "__main__":
+    print(main(part_one))
+    print(main(part_two))
